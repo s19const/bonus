be14d6aad145c292014c37487dab1c8466e81d03
const-test <kvshvarov@yandex.ru>
2022-06-12 21:48:21 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 100;  // Исходный счет
+        int vklad1 = 100; // Сумма пополнения на 100
+        int vklad2 = 1100; // Сумма пополнения на 1100
+
+
+
+        if (vklad1 > 1000) {
+            System.out.println(amount + vklad1);
+        } else {
+            System.out.println(amount + vklad2 + vklad2 / 100);
+        }
+    }
+}
+
+
+
+
